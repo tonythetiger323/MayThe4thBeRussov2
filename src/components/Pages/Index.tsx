8589b8f8297ddddae0d7354e6d741f83d9feddc7
Anthony Greeley <anthonygreeley2019@u.northwestern.edu>
2019-03-10 22:41:31 -0500

    made changes to Index.tsx

--- src/components/Pages/Index.tsx
+++ src/components/Pages/Index.tsx
@@ -1,31 +1,17 @@
 import * as React from 'react'
 import Page from '../Layouts/Page'
 import Typography from '@material-ui/core/Typography'
-import { withStyles, Theme } from '@material-ui/core/styles'
-
-const styles = (theme: Theme) => ({
-  stepper: {
-    height: '50px',
-    width: '300px',
-    opacity: 0.0
-  }
-})
 
 class Index extends React.Component {
   render() {
-    const style = {
-      fontFamily: 'Arvo'
-    }
     return (
       <div>
         <Page>
-          <Typography style={style} variant='h2'>
-            May the 4th be Russo
-          </Typography>
+          <Typography variant='h2'>May the 4th be Russo</Typography>
         </Page>
       </div>
     )
   }
 }
 
-export default withStyles(styles)(Index)
+export default Index
