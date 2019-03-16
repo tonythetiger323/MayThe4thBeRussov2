03c7d3bd96a79a1b67ee96bb578ed58ecf77ae39
Tony Greeley <tony.greeley@me.com>
2019-03-16 17:16:56 -0500

    updated switched from using npm to yarn, made changes to .nvmrc and package.json to reflect those changes. Did more work with tests and combined tsconfig.jest.json and tsconfig.test.json into one file.

--- src/components/NavbarLinkMenu/__tests__/index.test.tsx
+++ src/components/NavbarLinkMenu/__tests__/index.test.tsx
@@ -1,12 +1,73 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import NavbarLinkMenu from '../../NavbarLinkMenu';
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import NavbarLinkMenu from '../../NavbarLinkMenu'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import PopupState from 'material-ui-popup-state'
+import { createShallow } from '@material-ui/core/test-utils'
+import SomethingCrispy from '../../../__mocks__/SomethingCrispy'
+import SomethingExtraCrispy from '../../../__mocks__/SomethingExtraCrispy'
 
-describe('Navbar Component', () => {
-    let wrapper: any;
-    beforeEach(() => (wrapper = shallow(<NavbarLinkMenu />)));
+let wrapper: any
+let shallowMui: any
 
-    it('should render correctly', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-});
+describe('NavbarLinkMenu Component', () => {
+  beforeEach(() => (wrapper = shallow(<NavbarLinkMenu />)))
+
+  it('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('<Button/>', () => {
+  beforeEach(() => {
+    shallowMui = createShallow()
+  })
+
+  it('should work', () => {
+    wrapper = shallowMui(
+      <Button>
+        <SomethingCrispy />
+      </Button>
+    )
+  })
+})
+
+describe('<Menu/>', () => {
+  beforeEach(() => {
+    shallowMui = createShallow()
+  })
+
+  it('should work', () => {
+    wrapper = shallowMui(
+      <Menu open>
+        <SomethingCrispy />
+      </Menu>
+    )
+  })
+
+  describe('<MenuItem/>', () => {
+    beforeEach(() => {
+      shallowMui = createShallow()
+    })
+
+    it('should work', () => {
+      wrapper = shallowMui(<MenuItem />)
+    })
+  })
+
+  describe('<PopupState/>', () => {
+    beforeEach(() => {
+      shallowMui = createShallow()
+    })
+
+    it('should work', () => {
+      wrapper = shallowMui(
+        <PopupState variant='popover' popupId='NavbarLink-popup-menu'>
+          <SomethingExtraCrispy />
+        </PopupState>
+      )
+    })
+  })
+})
