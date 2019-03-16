5848a15cd53e3fc92901d5d573a3c1cb8e1e746f
Anthony Greeley <anthonygreeley2019@u.northwestern.edu>
2019-03-16 11:51:46 -0500

    setup more test files

--- src/components/NavbarLink/__tests__/index.test.tsx
+++ src/components/NavbarLink/__tests__/index.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { shallow } from 'enzyme'
 import NavbarLink from '../../NavbarLink'
 import { createShallow } from '@material-ui/core/test-utils'
+import Button from '@material-ui/core/Button'
 
 let wrapper: any
 let shallowMui: any
