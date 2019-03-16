f5a0ae49be086bfc298c744bc1e47df9ab8579b8
Tony Greeley <tony.greeley@me.com>
2019-03-16 08:34:31 -0500

    Progress on home page

--- src/Pages/UnderConstruction.tsx
+++ src/Pages/UnderConstruction.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react'
-import Page from '../Layouts/Page'
 import Typography from '@material-ui/core/Typography'
 
 const UnderConstruction = (): JSX.Element => {
-  return (
-    <div>
-      <Page>
-        <Typography variant='h1'>Under Construction</Typography>
-      </Page>
-    </div>
-  )
+  return <Typography variant='h1'>Under Construction</Typography>
 }
 
 export default UnderConstruction
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,27 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import AppliedRoute from './components/AppliedRoute'
-import Index from './components/Pages/Index'
-import UnderConstruction from './components/Pages/UnderConstruction'
-import NotFound from './components/Pages/NotFound'
+import AppliedRoute from './Components/AppliedRoute'
+import PageLayout from './Layouts/PageLayout'
+import Index from './Pages/Index'
+import UnderConstruction from './Pages/UnderConstruction'
+import NotFound from './Pages/NotFound'
 
 // component that will handle HTML routing
 export default () => {
   return (
-    <Switch>
-      <AppliedRoute exact path='/' component={Index} />
-      <AppliedRoute exact path='/theirstory' component={UnderConstruction} />
-      <AppliedRoute exact path='/herstory' component={UnderConstruction} />
-      <AppliedRoute exact path='/hisstory' component={UnderConstruction} />
-      <AppliedRoute exact path='/ceremony' component={UnderConstruction} />
-      <AppliedRoute exact path='/reception' component={UnderConstruction} />
-      <AppliedRoute exact path='/registry' component={UnderConstruction} />
-      <AppliedRoute exact path='/guestbook' component={UnderConstruction} />
-      <AppliedRoute exact path='/contactus' component={UnderConstruction} />
-      <Route component={NotFound} />
-    </Switch>
+    <PageLayout>
+      <Switch>
+        <AppliedRoute exact path='/' component={Index} />
+        <AppliedRoute exact path='/theirstory' component={UnderConstruction} />
+        <AppliedRoute exact path='/herstory' component={UnderConstruction} />
+        <AppliedRoute exact path='/hisstory' component={UnderConstruction} />
+        <AppliedRoute exact path='/ceremony' component={UnderConstruction} />
+        <AppliedRoute exact path='/reception' component={UnderConstruction} />
+        <AppliedRoute exact path='/registry' component={UnderConstruction} />
+        <AppliedRoute exact path='/guestbook' component={UnderConstruction} />
+        <AppliedRoute exact path='/contactus' component={UnderConstruction} />
+        <Route component={NotFound} />
+      </Switch>
+    </PageLayout>
   )
 }
