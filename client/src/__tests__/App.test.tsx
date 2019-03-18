a78dd37debab5438d365490873b8f81446e81938
Tony Greeley <tony.greeley@me.com>
2019-03-18 10:12:35 -0500

    updating react tests

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
+import { createShallow } from '@material-ui/core/test-utils/index'
 import App from '../App'
 import Routes from '../routes'
+import { MuiThemeProvider } from '@material-ui/core'
+import theme from '../theme/theme'
+import SomethingCrispy from '../__mocks__/SomethingCrispy'
 
 let wrapper: any
 
@@ -13,5 +17,21 @@ describe('App', () => {
 
   it('should render the Routes Component', () => {
     expect(wrapper.containsMatchingElement(<Routes />)).toEqual(true)
+  })
+})
+
+let shallowMui: any
+
+describe('<MuiThemeProvider />', () => {
+  beforeAll(() => {
+    shallowMui = createShallow()
+  })
+
+  it('should work', () => {
+    wrapper = shallowMui(
+      <MuiThemeProvider theme={theme}>
+        <SomethingCrispy />
+      </MuiThemeProvider>
+    )
   })
 })
