e9b2ab16b057e4dc06c2a715812643689e4a6fd5
Tony Greeley <tony.greeley@me.com>
2019-03-18 10:00:39 -0500

    removed typo from index.test.tsx

--- client/src/__tests__/index.test.tsx
+++ client/src/__tests__/index.test.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from '../App'
-import { AnyAaaaRecord } from 'dns'
 
 // tslint:disable-next-line: prefer-const
 let wrapper: any
