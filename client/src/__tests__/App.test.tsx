7b30cad655b55bb70bf3e6c421eeb44373e2d974
Tony Greeley <tony.greeley@me.com>
2019-03-18 10:06:01 -0500

    updating react tests

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
 import App from '../App'
+import Routes from '../routes'
 
 let wrapper: any
 
@@ -8,5 +9,9 @@ describe('App', () => {
   beforeEach(() => (wrapper = shallow(<App />)))
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1)
+  })
+
+  it('should render the Routes Component', () => {
+    expect(wrapper.containsMatchingElement(<Routes />)).toEqual(true)
   })
 })
