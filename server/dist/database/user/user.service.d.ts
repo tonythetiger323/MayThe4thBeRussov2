f14f1670882726bd01a0a69d8665ae44d8305cc2
Tony Greeley <tony.greeley@me.com>
2019-03-18 03:30:54 -0500

    updated server package.json

--- server/dist/database/user/user.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class UserService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    findAll(): Promise<User[]>;
-}
