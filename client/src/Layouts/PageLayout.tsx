b7afa46d64883d4c3e2ee6fed0c6c079c5d26f36
Tony Greeley <tony.greeley@me.com>
2019-03-19 05:19:19 -0500

    changed mvc structure

--- client/src/Layouts/PageLayout.tsx
+++ client/src/Layouts/PageLayout.tsx
@@ -3,12 +3,21 @@ import Typography from '@material-ui/core/Typography'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 
+const backgroundUrl =
+  'https://res.cloudinary.com/tonythetiger323/image/upload/e_blur:1196/v1550544815/MayThe4thBeRusso/Background Image/sean-o-406693-unsplash.jpg'
+const PageLayoutStyle = {
+  backgroundImage: `url(${backgroundUrl})`,
+  backgroundSize: 'cover',
+  overflow: 'hidden'
+}
 class PageLayout extends React.Component {
   render() {
     return (
-      <div className='app-container'>
+      <div className='app-container' style={PageLayoutStyle}>
         <header>
-          <Typography variant='h3' align='center'>May The 4th Be Russo</Typography>
+          <Typography variant='h3' align='center'>
+            May The 4th Be Russo
+          </Typography>
         </header>
         <nav>
           <Navbar />
