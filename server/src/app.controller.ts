1114d809d0e9b3843ccccdc56de1e24acd15c345
Tony <tony.greeley@me.com>
2019-03-22 01:14:34 -0500

    updated ci settings and started to impletement testing for api

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common'
+import * as path from 'path'
 
+const { PUBLIC_DIR, NODE_ENV } = process.env
+const PUBLIC_DIR_PATH = PUBLIC_DIR || 'client/public'
 @Controller()
 export class AppController {
   @Get()
   root(@Res() res) {
-    res.sendFile('index.html');
+    res.sendFile(
+      path.join(__dirname, '..', '..', `${PUBLIC_DIR_PATH}`, 'index.html')
+    )
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,13 @@
-import { ConfigService } from './../config/config.service'
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Connection } from 'typeorm'
-import { ConfigModule } from 'config/config.module'
+import { ConfigModule } from './config/config.module'
+import { UserModule } from './database/user/user.module'
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forRoot()],
-  controllers: [AppController],
+  imports: [ConfigModule, TypeOrmModule.forRoot(), UserModule],
+  controllers: [AppController]
 })
 export class AppModule {
   constructor(private readonly connection: Connection) {}
