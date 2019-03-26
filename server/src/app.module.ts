b5bc8b6c406a0e587dc8fcfa1c6c70889243c26f
Tony <tony.greeley@me.com>
2019-03-25 19:56:30 -0500

    started to setup database again

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from './config/config.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
+import { Connection } from 'typeorm'
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, TypeOrmModule.forRoot()],
   controllers: [AppController]
 })
 
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
