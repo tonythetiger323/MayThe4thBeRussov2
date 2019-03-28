b8ff55ff9b2073afbeb49085dcb41e44340b39d7
Tony <tony.greeley@me.com>
2019-03-28 12:59:00 -0500

    resetting up database, user table, and CRUD service

---
+++ server/src/database/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './user.entity'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async create(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+
+  async update(user: User): Promise<UpdateResult> {
+    return await this.userRepository.update(user.id, user);
+  }
+
+  async delete(id): Promise<DeleteResult> {
+    return await this.userRepository.delete(id);
+  }
+}
