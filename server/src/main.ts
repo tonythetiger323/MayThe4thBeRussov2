a4d58bd755bc1d232c8a4fbcb91aa2d6b35110f7
Tony <tony.greeley@me.com>
2019-03-30 22:54:49 -0500

    removed fastify adapter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import * as path from 'path';
 import * as favicon from 'serve-favicon';
@@ -10,7 +9,7 @@ const { PORT, PUBLIC_DIR, NODE_ENV } = process.env
 const PUBLIC_DIR_PATH = PUBLIC_DIR || 'client/public'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new FastifyAdapter())
+  const app = await NestFactory.create(AppModule)
 
   app.use(
     favicon(
@@ -26,7 +25,6 @@ async function bootstrap() {
   );
   app.use(bodyParser.json());
   app.use(express.static(path.join(__dirname, '..', '..', PUBLIC_DIR_PATH)));
-
 
   await app.listen(PORT || 3001, '0.0.0.0')
   console.log(`Server started on ${PORT || 3001}`)
