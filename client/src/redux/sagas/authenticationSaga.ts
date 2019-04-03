e6bacb52075ce1e29612c5d624cce3d7d028d22a
Miguel Caballero <mcabs3@gmail.com>
2019-04-03 08:08:05 -0700

    changed node version in package.json, added basic types for redux store, and adjusted api route for registration

--- client/src/redux/sagas/authenticationSaga.ts
+++ client/src/redux/sagas/authenticationSaga.ts
@@ -1,15 +1,14 @@
-import { put, call } from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects'
 import API from '../../utils/API'
 
 import * as types from '../actions'
+import { AxiosResponse } from 'axios'
 
 export function* registerSaga(payload: any) {
   try {
-    const response = yield call(API.registerUser, payload);
-    yield [
-      put({ type: types.REGISTER_USER_SUCCESS, response })
-    ];
-  } catch(error) {
-    yield put({ type: types.REGISTER_USER_ERROR, error });
+    const response: AxiosResponse = yield call(API.registerUser, payload)
+    yield put({ type: types.REGISTER_USER_SUCCESS, data: response.data })
+  } catch (error) {
+    yield put({ type: types.REGISTER_USER_ERROR, error })
   }
 }
---
+++ client/src/types/ApplicationState.ts
@@ -0,0 +1,5 @@
+import { RegisterState } from './RegisterState'
+
+export interface ApplicationState {
+  readonly register: RegisterState
+}
