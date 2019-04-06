b307b7bcebc1d75d0a3111c4eea604af3505aa50
Tony <tony.greeley@me.com>
2019-04-06 03:41:10 -0500

    updated tests

--- client/src/Components/NavbarLink/__tests__/index.test.tsx
+++ client/src/Components/NavbarLink/__tests__/index.test.tsx
@@ -3,7 +3,6 @@ import { shallow } from 'enzyme'
 import NavbarLink from '../../NavbarLink'
 import { createShallow } from '@material-ui/core/test-utils'
 import Button from '@material-ui/core/Button'
-import SomethingCrispy from '../../../__mocks__/SomethingCrispy'
 
 let wrapper: any
 let shallowMui: any
@@ -15,19 +14,5 @@ describe('NavbarLink Component', () => {
 
   it('should render correctly', () => {
     expect(wrapper).toMatchSnapshot()
-  })
-})
-
-describe('<Button/>', () => {
-  beforeEach(() => {
-    shallowMui = createShallow()
-  })
-
-  it('should work', () => {
-    wrapper = shallowMui(
-      <Button>
-        <SomethingCrispy />
-      </Button>
-    )
   })
 })
