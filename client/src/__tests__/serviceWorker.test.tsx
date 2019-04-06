38c0fd228ad0173e0a4fbb6692747ee8bb1a94b3
Tony <tony.greeley@me.com>
2019-04-06 00:26:15 -0500

    updated testing

--- client/src/__tests__/serviceWorker.test.tsx
+++
@@ -1,9 +0,0 @@
-import { isLocalhost } from '../serviceWorker'
-
-describe('isLocalHost', () => {
-  it('should be true if development', () => {
-    if (process.env.NODE_ENV === 'development') {
-      expect(isLocalhost).toEqual(true)
-    }
-  })
-})
