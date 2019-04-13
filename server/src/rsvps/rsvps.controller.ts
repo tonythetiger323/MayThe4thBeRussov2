87ef504afcffad59fc6149b4061942b4ee4080f6
Tony <tony.greeley@me.com>
2019-04-13 07:42:26 -0500

    updated RSVP to provide notification of successful or failed form submission

--- server/src/rsvps/rsvps.controller.ts
+++ server/src/rsvps/rsvps.controller.ts
@@ -10,7 +10,7 @@ export class RsvpsController {
   @Post()
   async create(@Param() params, @Res() res: Response, @Body() rsvpData: Rsvp): Promise<any> {
     console.log(`Request made to '/api/rsvps'`);
-    if (!(rsvpData && rsvpData.firstName && rsvpData.lastName && rsvpData.email && rsvpData.rsvp && rsvpData.guests && rsvpData.cars)) { return res.status(HttpStatus.FORBIDDEN).json({ message: 'RSVP, number of guests, and number of cars is required'});
+    if (!(rsvpData && rsvpData.firstName && rsvpData.lastName && rsvpData.email && rsvpData.rsvp && rsvpData.guests) && rsvpData.cars >= 0) { return res.status(HttpStatus.FORBIDDEN).json({ message: 'RSVP, number of guests, and number of cars is required'});
     };
     const rsvp = await this.rsvpsService.create(rsvpData);
     return res.status(HttpStatus.OK).json(rsvp)
