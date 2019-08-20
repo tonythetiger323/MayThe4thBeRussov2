1cd4813e40a71232d3feef06894335331e37ae73
Tony Greeley <tony.greeley@me.com>
2019-08-19 23:51:27 -0500

    updated tests files

--- client/src/Components/Footer/__tests__/index.test.tsx
+++ client/src/Components/Footer/__tests__/index.test.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import Footer from '../../Footer'
 import { createShallow } from '@material-ui/core/test-utils'
 import Paper from '@material-ui/core/Paper'
+import store from '../../../redux/store/index'
 
 let wrapper: any
 let shallowMui: any
 describe('Footer Component', () => {
-  const props = { isAuthenticated: false, logoutUser: '', user: 'test' }
-  beforeEach(() => (wrapper = mount(<Footer {...props} />)))
+  const props = { store, isAuthenticated: false, logoutUser: '', user: 'test' }
+  beforeEach(() => (wrapper = shallow(<Footer {...props} />)))
 
   it('should render correctly', () => {
     expect(wrapper).toMatchSnapshot()
