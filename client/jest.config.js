88909c89bb9b6de4ba516bdcb6a0e59de91b2e1e
Tony Greeley <tony.greeley@me.com>
2019-08-20 02:10:10 -0500

    Update CircleCI config

--- client/jest.config.js
+++ client/jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
       }
     ]
   ],
-  setupFiles: ['/src/setupTests.ts'],
+  setupFiles: ['./src/setupTests.ts'],
   snapshotSerializers: ['enzyme-to-json'],
   testEnvironment: 'jsdom',
   testMatch: [
